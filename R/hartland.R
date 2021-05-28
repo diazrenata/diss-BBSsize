ecb61e04cc41a0b4918dc7e9ae6de1248b0ebfdf
Renata Diaz <diaz.renatam@gmail.com>
2021-05-28 17:18:55 -0400

    sim methods

---
+++ R/hartland.R
@@ -0,0 +1,11 @@
+#' New Hartford BBS route.
+#'
+#' As loaded from MATSS (`get_bbs_route_region_data`). See MATSS for full documentation.
+#'
+#' @format A MATSS dataset.
+#' \describe{
+#'   \item{abundance}{tibble of species abundances (columns) per year (rows)}
+#'   \item{covariates}{tibble of covariate variables (columns) per year (rows)}
+#'   \item{metadata}{metadata list}
+#' }
+"hartland"
