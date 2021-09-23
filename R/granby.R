726e82c682c2727e0a12c955e4509ce9a92eb37a
Renata Diaz <diaz.renatam@gmail.com>
2021-09-23 17:47:09 -0400

    omg add granby

---
+++ R/granby.R
@@ -0,0 +1,11 @@
+#' Granby BBS route.
+#'
+#' As loaded from MATSS (`get_bbs_route_region_data`). See MATSS for full documentation.
+#'
+#' @format A MATSS dataset.
+#' \describe{
+#'   \item{abundance}{tibble of species abundances (columns) per year (rows)}
+#'   \item{covariates}{tibble of covariate variables (columns) per year (rows)}
+#'   \item{metadata}{metadata list}
+#' }
+"granby"
